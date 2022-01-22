3943b70e7b2934a772e85bfeb4edbbd26753f01b
Kyle Manning <k_manning@live.co.uk>
2022-01-22 18:28:36 +0000

    allow script to generate readme doc

--- scripts/create-challenge.sh
+++ scripts/create-challenge.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
-
+root=../../../..
 name=$1
 language=$2
+url=$3
 
-path=$name/$language
+simple_name=$(echo $name | sed "s/[^[:alnum:]]/ /g" | tr -s ' ')
+echo "Creating '$simple_name' folder for '$language'"
 
-mkdir -p "$path"
-cd "$path"
+src_path=$language/src
+folder_path=$simple_name/$src_path
 
-mkdir -p src
-touch src/Solution.$(echo $language | tr '[:upper:]' '[:lower:]') README.md
+# Create folder
+mkdir -p "$folder_path"
+cd "$simple_name"
+
+# Create src file
+case $language in
+    "JavaScript")
+        language=js
+        ;;
+    "Java")
+        language=java
+        ;;
+    *)
+        echo "unknown language '${language}' using default 'js'"
+        language=js
+esac
+
+touch "${src_path}/Solution.$language"
+
+# Create README for challenge statement
+kebab_name=$(echo $simple_name | tr '[:upper:]' '[:lower:]' | sed "s/ /-/g")
+python3 "$root/scripts/grab-problem.py" "$kebab_name" "$url"
