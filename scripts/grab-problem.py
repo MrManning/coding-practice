70476f4fbada7567564f81ea95468590a744420b
Kyle Manning <k_manning@live.co.uk>
2022-01-26 22:31:50 +0000

    allow command line arguments for grab problem script

--- scripts/grab-problem.py
+++ scripts/grab-problem.py
@@ -1,18 +1,29 @@
 import os
 from requests import get
 from bs4 import BeautifulSoup
-
-from cairosvg import svg2png
-import pytesseract
-import shutil
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM
-from reportlab.graphics import renderSVG
-from wand.api import library
-import wand.color
-import wand.image
-import pyvips
 import re
+import logging
+import argparse
+
+def init_argpase():
+    parser = argparse.ArgumentParser(description='Scrapes the url provided to get the challenges problem statement')
+    parser.add_argument('name', help="used as the default challenge name")
+    parser.add_argument('url', nargs="?", help="url override if name fails")
+    return parser
+
+def init_logger():
+    # create logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # create console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+
+    # add console handler to logger
+    logger.addHandler(console_handler)
+    return logger
 
 def write_to_file(file_name, contents):
     f = open(file_name, "w")
@@ -39,33 +50,42 @@ def get_html(url):
         challenge.append("\n")
 
     for title_div in problem_body.find_all("div", class_=re.compile("title$")):
-        title = f"### {title_div.text}"
+        title = f"### {title_div.text.strip()}\n"
         title_div.replace_with(title)
 
     for strong in problem_body.find_all("strong"):
-        title = f"\n\n### {strong.text}"
-        strong.replace_with(title)
+        titledd = f"\n\n### {strong.text}"
+        strong.replace_with(titledd)
 
     for span in problem_body.find_all("span", class_="MathJax_SVG"):
         span.replace_with("``")
 
     for li in problem_body.find_all("li"):
-        updated_list = f"- {li.text}"
+        updated_list = f"- {li.text.lstrip()}"
         li.replace_with(updated_list)
+
+    for group in problem_body.find_all(["ol", "ul"]):
+        group.replace_with(group.get_text().strip())
 
     for pre in problem_body.find_all("pre"):
         code = f"```\n{pre.text.strip()}\n```"
         pre.replace_with(code)
 
-    for p in problem_body.find_all("p"):
-        p.text.strip()
-
-    problem_body = re.sub(r'\n\n\n', '\n\n', problem_body.get_text())
+    problem_body = re.sub(r'[\n]{3,}', '\n\n', problem_body.get_text())
     problem_body = re.sub(r' +', ' ', problem_body)
 
     write_to_file("README.md", problem_body)
 
 
 if __name__ == "__main__":
-    # raw_html = get_html("https://www.hackerrank.com/challenges/simple-array-sum/problem")
-    raw_html = get_html("https://www.hackerrank.com/challenges/diagonal-difference/problem")
+    parser = init_argpase()
+    args = parser.parse_args()
+    logger = init_logger()
+
+    if not args.url:
+        logger.info("using name as url doesn't exist")
+        link = f"https://www.hackerrank.com/challenges/{args.name}/problem"
+        get_html(link)
+    else:
+        logger.info("using url as name override")
+        get_html(args.url)
